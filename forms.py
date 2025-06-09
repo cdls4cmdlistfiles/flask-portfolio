e9da397ea5f870b9aded061b38b38ddf8e5a452f
Gian <122416721+cdls4cmdlistfiles@users.noreply.github.com>
2025-06-09 17:59:02 +0800

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired()])
-    message = TextAreaField('Message', validators=[DataRequired()])
+    name = StringField('Name', 
+                      validators=[DataRequired()],
+                      render_kw={"placeholder": "Your name"})
+    
+    email = StringField('Email',
+                       validators=[DataRequired()],
+                       render_kw={"placeholder": "Your email address"})
+    
+    message = TextAreaField('Message',
+                          validators=[DataRequired()],
+                          render_kw={"placeholder": "Your message here..."})
+    
     submit = SubmitField('Submit')
